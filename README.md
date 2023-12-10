a60e88149464f8d68efc8e5140e7d135d04a348f
Gertjan Krediet <gj.krediet@gmail.com>
2023-12-10 10:48:17 +0100

    update to new version

--- README.md
+++ README.md
@@ -15,81 +15,18 @@ The app is rightie/leftie-proof
 
 ![20231111_191428](https://github.com/gjkrediet/Fossil_Dutch_Weather/assets/20277013/84026132-1ff0-40bb-9c3c-da1b29f1c778)
 
-The app exits after 30 seconds unless you hit the top buton. It is then locked. When locked, the hands show the actual time and the app retrieves information from the phone every minute. By pressing the top-button again you send a request tot Gadetbridge, asking for an immediate update of data from internet.
+The app exits after 30 seconds unless you press the top buton. It is then locked. When locked, the hands show the actual time and the app retrieves information from the phone every minute. By pressing the top-button again you send a request tot Gadetbridge, asking for an immediate update of data from internet.
 
-## App and Tasker
-The way I implemented it, the app relies on Tasker for retreiving information from the internet (buienradar and KNMI). The companion-app should send buienradar-data as a string without any delimiters or line-breaks. The various KNMI-data is sent in JSON format.
+### Installation
+Upload weatherApp.wapp to your watch or compile the app yourself. Instructions for preprocessing/compiling the app are similar to those for Gadgetbridge's opensourceWatchface which can be found here: https://codeberg.org/Freeyourgadget/fossil-hr-watchface
 
-### Dispatcher
-For the app to work you need to catch the 'RetrieveWeather' and 'RefreshWeather' in Tasker.
-For that create a profile in Tasker with the event "Intent Received" with the following content:
+Three Tasker tasks are included for retreivving and storing weather information. The main part of each task is a java scriptlet. One task is for downloading and storing buienradar-data, one for downloading and storing KNMI-data and one for retreiving the current location and city. Import the tasks in Tasker, change them to your needs and create a profile for each to run them at a regular interval. KNMI-data needs an API-key which you can obtain at weerlive.nl (for free for a limited update frequency) and insert into the http-request in tasker.
 
-Action: nodomain.freeyourgadget.gadgetbridge.Q_COMMUTE_MENU.
+A fourth taks contains the dispatcher for catching and processing requests from the watch. Create a profile in Tasker with the event "Intent Received" with the following content: Action: nodomain.freeyourgadget.gadgetbridge.Q_COMMUTE_MENU. Make this profile start the included 'dispatcher'-task. You may want to catch other requests from other apps, which you should then also include in this dispatcher (see https://codeberg.org/Freeyourgadget/Gadgetbridge/wiki/Fossil-Hybrid-HR#commute-app ).
 
-Make this profile start a 'dispatcher'-task with the following content:
-
-If: %extra_action EQ RefreshWeather
-
-	..... (send back intents to Gadgetbridge with data. See below)
- 
-Else if %extra_action EQ RetreiveWeather
-
-	..... (perform the task to download data from the internet. See below)
- 
-Endif
-
-You may want to catch other requests for other apps, which you should also include in this dispatcher (see https://codeberg.org/Freeyourgadget/Gadgetbridge/wiki/Fossil-Hybrid-HR#commute-app).
-
-Intents sent to Gadgetbridge are one with knmi-data and one with buienradar-data. 
-
-### knmi-data:
-
-Action: nodomain.freeyourgadget.gadgetbridge.Q_PUSH_CONFIG<br>
-
-Extra: EXTRA_CONFIG_JSON:{"push":{"set":{"weatherApp._.config.knmi": %Knmidata}}}
-
-The (global) variable %Knmidata should contain a JSON-string like this: {"temp":"8.3","time":"03-11-2023 08:53","plaats":"Amsterdam","d0tmin":"7","d0tmax":"11","d0neerslag":"13","d1tmin":"7","d1tmax":"11","d1neerslag":"90","d2tmin":"9","d2tmax":"12","d2neerslag":"90"}
-
-### buienradar-data:
-
-Action: nodomain.freeyourgadget.gadgetbridge.Q_PUSH_CONFIG
-
-Extra: EXTRA_CONFIG_JSON:{"push":{"set":{"weatherApp._.config.buienradar":{"rawtxt":"%Brdata"}}}}
-	
-The (global) variable %Brdata should contain a string containing the data from buienradar with no delimiters or linebreaks. Thus it contains 7 characters (3 for precipitation, 4 for the time in hhmm) for each 5 minutes of data. It looks like this: 000151000015150401520040152500015300001535000154000015450001550120155515016000901605080161000016150001620000162500016300001635000164000016450001650000165500017000901705
-
-### Examples
-Three Tasker tasks are included as an example how to achieve the above. One for downloading buienradar-data and storing it in %Brdata, one for downloading KNMI-data and storing it in %Knmidata. Import them in Tasker, change them to your needs and create a profile for each to run them at a regular interval. KNMI-data needs an API-key which you need to obtain at weerlive.nl (for free for a limited update frequency) and insert into the http-request in tasker. The 'dispatcher' task is also included. 
-
-The source of the app is included as well as the wapp-file
-
-## Widget
-The source for a buienradar complication/widget is also included. It shows an icon (raindrop indicating rain within next 15 minutes or an umbrealla indicating rain witin the next two hours) and the maximum precipation. 
-![20231103_095239](https://github.com/gjkrediet/Fossil_Dutch_Weather/assets/20277013/48a5caf3-940a-4c5f-a4a4-0ec001263711)
-You can preprocess/compile/pack it together with a watchface. In de layout-file for the watchface (..\build\files\config\customWatchface) you should include a section like this:
-{
-"type": "comp",
- "name": "widgetBr",
- "goal_ring": false,
- "color": "white",
- "data": {
- "update_timeout": 60,
- "timeout_hide_text": false,
- "timeout_show_circle": true
- },
- "size": {
- "w": 76,
- "h": 76
- },
- "pos": {
- "x": 120,
- "y": 180
- }
-}
-
-To update de widget you should send data from Tasker to Gadgetbridge:
-Action: nodomain.freeyourgadget.gadgetbridge.Q_PUSH_CONFIG
-Extra: EXTRA_CONFIG_JSON:{"push":{"set":{"widgetBr._.config.brdata":"%Brdata"}}}
+## Changes since the previous version
+To enable the use of a variety of weather -providers, the information in the intents is changed and intents for current weather-info and for the forecast are splitted. This has minimal effect on the funtionality and appearance. Time of the forecast is not shown anymore.
+A crude check on the validity of the raindata is now done. When data is more than a quarter out of range, questionmarks are shown in the graph. Data from buienradar does not incorporate a timestamp, therefore a watertight check is impossible.
 
 ## Credits
 Many thanks to https://codeberg.org/arjan5 and https://github.com/dakhnod for their insights and help. This app is initially based on their examples and work and in part made with their tools.
--- app.js
+++ app.js
@@ -9,19 +9,21 @@ return {
     },
     config: {
 		knmi: undefined,
-		buienradar: undefined
+		openmeteo: undefined
 	},
     timeout_timetelling: 30 * 1000,
     timeout_retrieve: 60 * 1000,
 	lock: false,
 	working: false,
 	current_temp: -99,
+	full_refresh_needed: true,
     handler: function (event, response) {
         this.wrap_event(event)
         this.wrap_response(response)
         this.state_machine.handle_event(event, response)
     },
     log: function (object) {
+		object.logentry_from = this.node_name;
         req_data(this.node_name, '"type": "log", "data":' + JSON.stringify(object), 999999, true)
     },
 	weather_retrieve: function() {
@@ -30,66 +32,100 @@ return {
  		start_timer(this.node_name, 'retrieve_timer', this.timeout_retrieve)
 	},
 	weather_refresh: function() {
-        req_data(this.node_name, '"commuteApp._.config.commute_info":{"dest":"UpdateWeather","action":"start"}', 999999, true)
+		req_data(this.node_name, '"commuteApp._.config.commute_info":{"dest":"UpdateWeather","action":"start"}', 999999, true);
 	},
-    draw_info: function (response) {
-		
-		response.draw = {}
+    draw_info: function (response) {		
+        response.draw = {
+            node_name: this.node_name,
+            package_name: this.package_name,
+            layout_function: 'layout_parser_json',
+            background: get_common().U('INVERTED')?'BGIweather.raw':'BGweather.raw',
+            array: [],
+            update_type: this.full_refresh_needed ? 'gc4' : 'du4',
+            skip_invert: true,
+        };
 		var layout_info = {
-			json_file: 'weather_layout'
+			json_file: 'weather_layout',
 		}
 		
-		if (this.config.buienradar != undefined && this.config.buienradar.rawtxt.length == 168) {
-			var brdata=this.config.buienradar.rawtxt	
-			var brhour=parseInt(brdata.slice(3,5))
-			var brminutes=parseInt(brdata.slice(5,7))
-			var intensity
+		if (this.config.weather_rain != undefined) {
+			var raindata=this.config.weather_rain.raindata	
+			var intensity;
+			var resolution = this.config.weather_rain.resolution; //number of datapoints
+			var span = this.config.weather_rain.span; //total minutes of the series
+			var step_x = Math.floor(156/resolution); //number of pixels per datapoint
+			
 
-			for (var i=0;i<12;++i) {
-				intensity = Math.max(brdata.substring(i*14,i*14+3),brdata.substring(i*14+7,i*14+10))
-				if (intensity>77) {
-					intensity = 3 + (intensity - 77) * 0.57
-				} else if (intensity>0) {
-					intensity=2
+			for (var i=0;i<resolution;++i) {
+				intensity = raindata.substring(i*3,i*3+3);
+				if (intensity<10) {
+					intensity*=2;
+				} else if (intensity <100) {
+					intensity=(intensity-10)/3+20;
 				} else {
-					intensity=1
+					intensity=(intensity-100)/60 + 60
 				}
-				intensity=Math.round(intensity)
-				layout_info['h_' + i] = intensity
-				layout_info['y_' + i] = 80 - intensity
-				layout_info['x_' + i] = 41 + i * 13
+				intensity+=1;
+				intensity = Math.round(intensity);
+				layout_info['h_' + i] = intensity;
+				layout_info['y_' + i] = 80 - intensity;
+				layout_info['x_' + i] = 44 + i * step_x;
 			}
-
-			layout_info['w'] = 12
-			brhour+=1;
-			brhour%=24;
-			layout_info['marker1_pos'] = 21 + 6*13-((brminutes/5)*7)
-			layout_info['marker1'] = '|\n' + brhour + ':00'
-			brhour+=1;
-			brhour%=24;
-			layout_info['marker2_pos'] = 21 + 12*13-((brminutes/5)*7)
-			layout_info['marker2'] = '|\n' + brhour + ':00'
+			layout_info['w'] = step_x - 1;
+			var now_minutes = 60 * get_common().hour + get_common().minute - get_common().minute%5;
+			var start_minutes = this.config.weather_rain.start;
+			//check the difference between start of the series and current time. First check times around midnight
+			if (now_minutes > 1425 && start_minutes < 15) {
+				if ((1440 - now_minutes + start_minutes) > 15) start_minutes = -1;
+			}
+			else if (start_minutes > 1425 && now_minutes < 15) {
+				if ((1440 - start_minutes + now_minutes) > 15) start_minutes = -1;
+			}
+			else if ((start_minutes > now_minutes + 15) || (start_minutes < now_minutes - 15)) start_minutes = -1;
+			if (start_minutes < 0) {
+				layout_info['marker1_xpos'] = 70;
+				layout_info['marker2_xpos'] = 100;
+				layout_info['marker_ypos'] = 53;
+				layout_info['marker1'] = '? ?'
+				layout_info['marker2'] = '? ?'
+			} else {
+				var rainhour=Math.floor(this.config.weather_rain.start/60);
+				var rainminute=this.config.weather_rain.start%60;
+				layout_info['marker_ypos'] = 74;
+				var hour;
+				rainhour=(++rainhour)%24;
+				// create leading zero
+				hour ='0' + rainhour;
+				hour = hour.substring(hour.length-2,hour.length);
+				layout_info['marker1_xpos'] = 23 + ((60-rainminute)/60)*step_x*60*resolution/span;
+				layout_info['marker1'] = '|\n' + hour + ':00';
+				rainhour=(++rainhour)%24;
+				hour ='0' + rainhour;
+				hour = hour.substring(hour.length-2,hour.length);
+				layout_info['marker2_xpos'] =  23 + ((120-rainminute)/60)*step_x*60*resolution/span;
+				layout_info['marker2'] = '|\n' + hour + ':00';
+			}
 		}
-		
-		if (this.config.knmi != undefined) {
-			this.current_temp = this.config.knmi.temp
-			layout_info['temp_cur'] = this.current_temp + '°C'
-			layout_info['plaats_tijd'] = this.config.knmi.plaats + ' - ' + this.config.knmi.time.substring(11,16)
-			layout_info['d0'] = 'max: ' + this.config.knmi.d0tmax + '\n' + 'min:  ' + this.config.knmi.d0tmin + '\n' + 'rgn:   ' + this.config.knmi.d0neerslag
-			layout_info['d1'] = this.config.knmi.d1tmax + '\n' + this.config.knmi.d1tmin + '\n' + this.config.knmi.d1neerslag
-			layout_info['d2'] = this.config.knmi.d2tmax + '°C\n' + this.config.knmi.d2tmin + '°C\n' + this.config.knmi.d2neerslag + '%'
+		if (this.config.weather_current != undefined) {
+			layout_info['plaats'] = this.config.weather_current.provider 
+				+ ' | ' + this.config.weather_current.city
+				+ ' | ' + this.config.weather_current.current_temp + '°C';
+		}		
+		if (this.config.weather_forecast != undefined) {
+			layout_info['d0'] = 'max: ' + Math.round(this.config.weather_forecast.d0tmax) + '\n' + 'min:  ' + Math.round(this.config.weather_forecast.d0tmin) + '\n' + 'rgn:   ' + Math.ceil(this.config.weather_forecast.d0rain);
+			layout_info['d1'] = Math.round(this.config.weather_forecast.d1tmax) + '\n' + Math.round(this.config.weather_forecast.d1tmin) + '\n' + Math.ceil(this.config.weather_forecast.d1rain);
+			layout_info['d2'] = Math.round(this.config.weather_forecast.d2tmax) + '°C\n' + Math.round(this.config.weather_forecast.d2tmin) + '°C\n' + Math.ceil(this.config.weather_forecast.d2rain) + this.config.weather_forecast.unit_rain;
 		}
-
-		layout_info['background'] = 'weatherBG'
-		layout_info['button_middle'] = 'icHome'
-		layout_info['button_top'] = this.lock ? (this.working ? '' : 'icRefresh') : 'icLock'
-		layout_info['button_top_bot_x'] = 198
-		layout_info['button_middle_x'] = 210
+		layout_info['button_middle'] = 'icHome';
+		layout_info['button_top'] = this.lock ? (this.working ? '' : 'icRefresh') : 'icLock';
+		layout_info['button_top_bot_x'] = get_common().U('WATCH_MODE')=='LEFTIE'? 22 : 198;
+		layout_info['button_middle_x'] = get_common().U('WATCH_MODE')=='LEFTIE'? 10 : 210;
+		layout_info['invert'] = get_common().U('INVERTED');
 		response.draw[this.node_name] = {
 			'layout_function': 'layout_parser_json',
 			'layout_info': layout_info,
 		}
-//		response.emulate_double_tap()
+		this.full_refresh_needed=false;
 	},
     wrap_state_machine: function(state_machine) {
         state_machine.set_current_state = state_machine.d
@@ -97,7 +133,6 @@ return {
         state_machine.get_current_state = function(){
             return state_machine.n
         }
-
         return state_machine
     },
     wrap_event: function (system_state_update_event) {
@@ -138,6 +173,7 @@ return {
         return response
     },
     handle_global_event: function (self, state_machine, event, response) {
+        self.log(event)
         if (event.type === 'system_state_update' && event.concerns_this_app === true) {
             if (event.new_state === 'visible') {
                 self.state_machine.set_current_state('weather_info');
@@ -147,17 +183,17 @@ return {
         } else if(event.type == 'node_config_update' && event.node_name == self.node_name) {
             if (self.state_machine.get_current_state() == 'weather_info') {
 				self.working=false
-				self.draw_info(response) // Arjan5: Makes only sense if visible
+				self.draw_info(response)
 			}
         } else if (event.type === 'middle_hold') {
             response.go_home(true)
         } else if (event.type == 'timer_expired' && is_this_timer_expired(event, self.node_name, 'timetelling_timer')) {
 			if (self.lock) {
 				var hands = enable_time_telling();
-				response.move_hands(hands.hour_pos, hands.minute_pos, false)
+				response.move_hands(hands.hour_pos, hands.minute_pos, false);
 				self.time_telling_enabled = true;
 			} else {
-				response.go_home(true)
+				response.go_home(true);
 			}
         }
     },
@@ -202,8 +238,8 @@ return {
 							stop_timer(self.node_name, 'timetelling_timer')
 							disable_time_telling();
 							self.weather_retrieve();
-							response.move_hands(270, 270, false)
-							start_timer(self.node_name, 'timetelling_timer', self.timeout_timetelling)
+							response.move_hands(270, 270, false);
+							start_timer(self.node_name, 'timetelling_timer', self.timeout_timetelling);
 						}
 					}
                 }
